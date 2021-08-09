745e8a33736cf4fe48ef7b4dd97c764442761995
BlueBurgersTDD <58081413+TheBlueBurger@users.noreply.github.com>
2021-08-09 17:25:37 +0200

    Hopefully this works

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
+process.on("uncaughtException", err => {
+	try {
+		console.log(chalk.bold.red("An unexpected error occured! Technical details:\n" + err.message));
+		inquirer.prompt({
+			name: "exit",
+			message: "What do you want to do?",
+			type: "list",
+			choices: [{
+					name: "Write to log and exit",
+					value: "write_log"
+				},
+				{
+					name: "Exit",
+					value: "exit"
+				}
+			]
+		}).then(out => {
+			if (out.exit == "exit") process.exit(1)
+			else if (out.exit == "write_log") {
+				fs.writeFileSync("saladbind_error.txt", `An error occured.\nError: ${err}\n\nStacktrace: ${err.stack}`);
+				process.exit(1);
+			}
+		})
+	} catch (newError) {
+		console.log("ERROR: ", {
+			err,
+			newError
+		});
+		process.exit(1);
+	}
+});
+
 const ora = require('ora'); // ara ara
 const chalk = require('chalk');
 const packageJson = require('./package.json');
